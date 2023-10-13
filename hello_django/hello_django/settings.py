ce588adab931dabc736b65677ecce655fc8f35a2
Maimouna DIOUF <maimouna0398@gmail.com>
2023-10-13 13:47:52 +0000

    host

--- hello_django/hello_django/settings.py
+++ hello_django/hello_django/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['doctor-app-h45i.onrender.com']
 
 
 # Application definition
