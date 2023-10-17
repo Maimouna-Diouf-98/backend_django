0af84cf6d4bbe1e635aab0e811ce2ed501294e03
Maimouna DIOUF <maimouna0398@gmail.com>
2023-10-17 12:34:07 +0000

    finish doctor

--- hello_django/doctor/urls.py
+++ hello_django/doctor/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('create-doctor/', views.CreateDoctorAPI.as_view()),
     path('update-doctor/<int:pk>/',  views.UpdateDoctorAPI.as_view()),
     path('list-id-doctor/<int:pk>/',  views.list_id_doctor),
-    # path('list-doctor/', ListDoctor.as_view(), name='list-doctor'),
-    # path('create-jour/', Jour.as_view(), name='create-jour'),
-    # path('create-heure/', Heure.as_view(), name='create-heure'),
-    # path('create-jour-heure/', JourEtHeure.as_view(), name='create-jour-heure'),
+    path('delete-doctor/<int:pk>/',  views.delete_doctor),
+    path('up-id-doctor/<int:pk>/',  views.update_id_doctor),
 ]
--- hello_django/doctor/views.py
+++ hello_django/doctor/views.py
@@ -11,28 +11,60 @@ from rest_framework import status
 
 
 # # Create your views here.
+#  create doctor
+class CreateDoctorAPI(CreateAPIView):
+    queryset = CustomDoctor.objects.all()
+    serializer_class = CreateDoctorSerializer
+    premission_classes = (AllowAny,)
 
-@api_view(['GET', 'POST','PUT', 'DELETE'])
+#  get all doctors
+@api_view(['GET'])
 def doctors_list(request):
     if request.method == 'GET':
         doctor = CustomDoctor.objects.all()
         serializer = DoctorSerializer(doctor, many=True)
         return Response(serializer.data)
+
+#  get one doctor
+@api_view(['GET'])
 def list_id_doctor(request, pk):
     try:
         doctor = CustomDoctor.objects.get(pk=pk)
-        serializer = DoctorSerializer(doctor, many=True)
-    except CustomDoctor.DoesNotExist: 
+    except doctor.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-    
-        return Response(serializer.data,content_type="application/json")
+        serializer = DoctorSerializer(doctor)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
-class CreateDoctorAPI(CreateAPIView):
-    queryset = CustomDoctor.objects.all()
-    serializer_class = CreateDoctorSerializer
-    premission_classes = (AllowAny,)
+#  update a doctor
+@api_view(['PUT'])
+def update_id_doctor(request, pk):
+    try:
+        doctor = CustomDoctor.objects.get(pk=pk)
+    except doctor.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = DoctorSerializerr(doctor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#  update doctor
 class UpdateDoctorAPI(UpdateAPIView):
     queryset = CustomDoctor.objects.all()
     serializer_class = UpdateDoctorSerializer
+    
+#  delete a doctor
+@api_view(['DELETE'])
+def delete_doctor(request, pk):
+    try:
+        doctor = CustomDoctor.objects.get(pk=pk)
+    except doctor.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'DELETE':
+        doctor.delete()
+
